1097fd53e15265a9213cb86243e8e2f429da5060
Mohit Kumar Prasad <it.20203040@gmail.com>
2023-08-23 23:57:13 +0530

    Hi

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,19 @@
+const Navigation = () => {
+  return (
+    <div>
+      <nav className="container">
+        <div className="logo">
+          <img src="../public/images/brand_logo.png" alt="Loading!!!" />
+        </div>
+        <ul>
+          <li href="#">Menu</li>
+          <li href="#">Location</li>
+          <li href="#">About</li>
+          <li href="#">Contact</li>
+        </ul>
+        <button>Login</button>
+      </nav>
+    </div>
+  );
+};
+export default Navigation;
